a7d8bb8645a2eb6dae005184e0fdc0b5f5761937
Bruno Eduardo <bruno@beduardo.com>
2018-11-14 00:31:07 -0200

    add more items

--- app/index.js
+++ app/index.js
@@ -4,4 +4,7 @@ var ngModule = angular.module('marvelApp', []);
 
 
 import {MDCRipple} from '@material/ripple';
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 
+import {MDCTextField} from '@material/textfield';
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));

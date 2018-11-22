74a4c9782c2b8e5e1422f58a91bfafa10e6f0089
Bruno Eduardo <bruno@beduardo.com>
2018-11-21 23:07:05 -0200

    webpack modules

---
+++ app/common/directives/card/card.js
@@ -0,0 +1,13 @@
+
+// import marvelModule from 'index';
+
+// marvelModule.directive('card', function() {
+
+//   return {
+//         restrict: 'E',
+//         replace: true,
+//         templateUrl: 'card.html',
+//   };
+// });
+
+alert("ok");
--- app/index.js
+++ app/index.js
@@ -1,7 +1,24 @@
+import angular from 'angular';
+import route from 'angular-route';
 
-var angular = require('angular');
-var ngModule = angular.module('marvelApp', []);
+import route from 'angular-route';
 
+require('common/directives/card/card');
+
+var marvelModule = angular.module('marvelApp', [route]);
+
+marvelModule.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "pages/home/home.html"
+    })
+    .when("/comics", {
+        templateUrl : "pages/comics/comics.html"
+    })
+    .when("/characters", {
+        templateUrl : "pages/characters/characters.html"
+    })
+});
 
 import {MDCRipple} from '@material/ripple';
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));

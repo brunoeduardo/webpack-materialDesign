58d28fda2f4fa881296d0f26d7e87066bd345fde
Bruno Eduardo <bruno.eduardo@netbiis.com>
2018-11-30 15:43:27 -0200

    add marvel API

--- app/app.js
+++ app/app.js
@@ -17,6 +17,13 @@ const marvelModule = angular.module('marvelApp', [route, card, search])
     .name
 
 
+    https://developer.marvel.com/docs#!/public/getComicsCollection_get_6
 
+    http://gateway.marvel.com/v1/public/characters?ts=1&apikey=47fbbb8ce97fc46360c96f01d5179835&hash=74a5d628d3e86d97d2f3ebfbf02b2ebd
 
-
+    Your public key
+    47fbbb8ce97fc46360c96f01d5179835
+    Your private key
+    3bf5a08c7453a44a3f164719299a9143d5abb003
+    hash
+    74a5d628d3e86d97d2f3ebfbf02b2ebd
